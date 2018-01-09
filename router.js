7e9141a79c4465ead246ce48a259eef95ec237fc
Vernon Mensah <vernonmensah@gmail.com>
2018-01-09 14:48:57 -0500

    Finished all paths

--- router.js
+++ router.js
@@ -8,17 +8,25 @@ mongoose.Promise = global.Promise;
 
 const { Post } = require('./models');
 
+function seperateName (fullName) {
+    return fullName.split(' ');
+}
+
+function printPretty(items){
+  let sample = items.map((item) => {
+    return item.serialize();
+  })
+
+  let output = JSON.stringify(sample,null,4);
+  console.log(output);
+}
+
 router.get('/posts', (req, res) => {
   Post
     .find()
     .then(posts => {
 
-      let sample = posts.map((post) => {
-        return post.serialize();
-      })
-
-      let output = JSON.stringify(sample,null,4);
-      console.log(output);
+      printPretty(posts);
 
       res.json({
         posts: posts.map(
@@ -28,76 +36,83 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/posts/:id', (req, res) => {
+  let id = req.params.id;
+
   Post
-    .find()
-    .then(posts => {
+    .findById(id)
+    .then(post => {
 
-      let sample = posts.map((post) => {
-        return post.serialize();
-      })
+      res.json(post.serialize());
 
-      let output = JSON.stringify(sample,null,4);
-      console.log(output);
-
-      res.json({
-        posts: posts.map(
-          (post) => post.serialize())
-      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
     });
 });
 
 router.post('/posts', (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
   Post
-    .find()
-    .then(posts => {
-
-      let sample = posts.map((post) => {
-        return post.serialize();
-      })
-
-      let output = JSON.stringify(sample,null,4);
-      console.log(output);
-
-      res.json({
-        posts: posts.map(
-          (post) => post.serialize())
-      });
+    .create({
+      author: {
+        firstName: req.body.author.firstName,
+        lastName: req.body.author.lastName,
+      },
+      content: req.body.content,
+      title: req.body.title
+    })
+    .then(post => res.status(201).json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
     });
+
+
 });
 
 router.put('/posts/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({ message: message });
+  }
+
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'author.firstName', 'author.lastName'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
   Post
-    .find()
-    .then(posts => {
-
-      let sample = posts.map((post) => {
-        return post.serialize();
-      })
-
-      let output = JSON.stringify(sample,null,4);
-      console.log(output);
-
-      res.json({
-        posts: posts.map(
-          (post) => post.serialize())
-      });
-    });
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .then(post=> res.status(200).json(post))
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
 router.delete('/posts/:id', (req, res) => {
   Post
-    .find()
-    .then(posts => {
-
-      let sample = posts.map((post) => {
-        return post.serialize();
-      })
-
-      let output = JSON.stringify(sample,null,4);
-      console.log(output);
-
-      res.status(204).end();
-    });
+    .findByIdAndRemove(req.params.id)
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
 module.exports = router;
