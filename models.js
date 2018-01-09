74b7766224d011d5ada74b08f07e5b407f94bfed
David Park <dysp92@gmail.com>
2018-01-09 12:40:26 -0800

    finished endpoints

--- models.js
+++ models.js
@@ -17,8 +17,18 @@ const postSchema = mongoose.Schema({
 // properties that are stored in the database. Here we use it
 // to generate a human readable string based on the address object
 // we're storing in Mongo.
-postSchema.virtual('authorName').get(function() {
-  return `${this.author.firstName} ${this.author.lastName}`.trim();});
+postSchema
+  .virtual('authorName')
+  .get(function() {
+    return `${this.author.firstName} ${this.author.lastName}`.trim();
+  })
+  .set(function(authorName) {
+    // To set the full name we want to split it up, and put the right
+    // parts in the firstName and lastName fields
+    const [first, last] = authorName.split(' ');
+    this.author.firstName = first;
+    this.author.lastName = last;
+  });
 
 // this is an *instance method* which will be available on all instances
 // of the model. This method will be used to return an object that only
@@ -33,6 +43,8 @@ postSchema.methods.serialize = function() {
     created: this.created
   };
 };
+
+
 
 // note that all instance methods and virtual properties on our
 // schema must be defined *before* we make the call to `.model`.
--- router.js
+++ router.js
@@ -9,13 +9,13 @@ mongoose.Promise = global.Promise;
 const { Post } = require('./models');
 
 function seperateName (fullName) {
-    return fullName.split(' ');
+  return fullName.split(' ');
 }
 
 function printPretty(items){
   let sample = items.map((item) => {
     return item.serialize();
-  })
+  });
 
   let output = JSON.stringify(sample,null,4);
   console.log(output);
@@ -52,7 +52,7 @@ router.get('/posts/:id', (req, res) => {
 });
 
 router.post('/posts', (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
+  const requiredFields = ['title', 'content', 'authorName'];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -64,10 +64,7 @@ router.post('/posts', (req, res) => {
 
   Post
     .create({
-      author: {
-        firstName: req.body.author.firstName,
-        lastName: req.body.author.lastName,
-      },
+      authorName: req.body.authorName,
       content: req.body.content,
       title: req.body.title
     })
@@ -92,18 +89,22 @@ router.put('/posts/:id', (req, res) => {
   // we only support a subset of fields being updateable.
   // if the user sent over any of the updatableFields, we udpate those values
   // in document
-  const toUpdate = {};
-  const updateableFields = ['title', 'content', 'author.firstName', 'author.lastName'];
+  let toUpdate = {};
 
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      toUpdate[field] = req.body[field];
+  let [first, last] = seperateName(req.body.authorName);
+  console.log(first, last);
+  toUpdate = {
+    title: req.body.title,
+    content: req.body.content,
+    author: {
+      firstName: first,
+      lastName: last
     }
-  });
+  };
 
   Post
     // all key/value pairs in toUpdate will be updated -- that's what `$set` does
-    .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+    .findByIdAndUpdate(req.params.id, { $set: toUpdate})
     .then(post=> res.status(200).json(post))
     .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
